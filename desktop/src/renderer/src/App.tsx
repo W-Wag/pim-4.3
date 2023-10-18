1d68b5225ecd957233852a4a191d860c866c4491
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-10-18 17:17:22 -0300

    Instalando o shadcn ui no projeto desktop

--- desktop/src/renderer/src/App.tsx
+++ desktop/src/renderer/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button'
+
 export function App(): JSX.Element {
   return (
     <div className="container">
       <h1>Hello World</h1>
+      <Button>ello</Button>
     </div>
   )
 }
