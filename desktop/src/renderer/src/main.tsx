bbdc6109f7f2038e05030b472c967d002f0f28ca
W-Wag <wagner.oliv.m@gmail.com>
2023-10-19 20:34:46 -0300

    Criando a home

--- desktop/src/renderer/src/main.tsx
+++ desktop/src/renderer/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import { App } from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Header } from './components/header/header'
+import { Home } from './Home'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
