3eb947b021bf2cc31990f6a9813d39315ab9e2b4
W-Wag <wagner.oliv.m@gmail.com>
2023-10-20 21:38:58 -0300

    Criando o form de criação do aluno

--- desktop/src/renderer/src/components/CreateStudent.tsx
+++ desktop/src/renderer/src/components/CreateStudent.tsx
@@ -13,27 +13,32 @@ import { Input } from './ui/input'
 import { useForm } from 'react-hook-form'
 import { Button } from './ui/button'
 import { ReactElement } from 'react'
-import { Link } from 'react-router-dom'
+import { IMaskInput } from 'react-imask'
 
 const formSchema = z.object({
-  cpf: z.string().min(11, {
-    message: 'CPF inválido, digite novamente'
-  }),
+  cpf: z
+    .string()
+    .min(11, {
+      message: 'CPF inválido, digite novamente'
+    })
+    .max(11, {
+      message: 'CPF inválido, digite novamente'
+    }),
   nome: z.string(),
   dt_nascimento: z.string().regex(/{1-9}-{1-9}-{1-9}{1-9}/, {
     message: 'Data nascimento deve ser do tipo DD-MM-AA'
   }),
-  rg: z.string().min(2, {
-    message: 'Password must be at least 2 characters.'
+  rg: z.string().min(5, {
+    message: 'Rg deve ter no mínimo 5 dígitos'
   }),
-  telefone: z.string().min(2, {
-    message: 'Password must be at least 2 characters.'
+  telefone: z.string().min(9, {
+    message: 'Telefone deve ter no mínimo 9 dígitos'
   }),
-  telefone2: z.string().min(2, {
-    message: 'Password must be at least 2 characters.'
+  telefone2: z.string().min(9, {
+    message: 'Telefone deve ter no mínimo 9 dígitos'
   }),
-  genero: z.string().min(2, {
-    message: 'Password must be at least 2 characters.'
+  genero: z.string().regex(/[H|M]/g, {
+    message: 'Tipo de gênero inválido'
   })
 })
 
@@ -70,7 +75,12 @@ export function CreateStudent(): JSX.Element {
               <FormItem>
                 <FormLabel>CPF</FormLabel>
                 <FormControl>
-                  <Input className="w-96" placeholder="111-111-111.40" {...field} />
+                  <IMaskInput
+                    className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                    placeholder="111-111-111.40"
+                    mask="000.000.000-00"
+                    {...field}
+                  />
                 </FormControl>
                 <FormDescription>Insira o CPF do aluno</FormDescription>
                 <FormMessage />
@@ -91,9 +101,21 @@ export function CreateStudent(): JSX.Element {
               </FormItem>
             )}
           />
-          <Button type="submit">
-            <Link to="/home">Confirmar</Link>
-          </Button>
+          <FormField
+            control={form.control}
+            name="genero"
+            render={({ field }): ReactElement => (
+              <FormItem>
+                <FormLabel>Gênero</FormLabel>
+                <FormControl>
+                  <Input className="w-96" placeholder="Insira o gênero" {...field} />
+                </FormControl>
+                <FormDescription>Insira H - Homem ou M - Mulher</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Confirmar</Button>
         </form>
       </Form>
     </div>
