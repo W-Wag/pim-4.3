e135f72a3e7831357275c0fcc4a69091bb4f5308
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-11-08 17:00:00 -0300

    Aplicando os dados do back end na área alunos do web

--- web/src/components/school-records/schoolRecords.tsx
+++ web/src/components/school-records/schoolRecords.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from 'react';
 import {
   Table,
   TableBody,
@@ -6,8 +7,42 @@ import {
   TableHeader,
   TableRow,
 } from '../ui/table';
+import { api } from '@/lib/api';
+import { Skeleton } from '../ui/skeleton';
+
+interface Records {
+  nota: {
+    np1: number;
+    np2: number;
+    pim: number;
+    mf: number;
+  };
+  disciplina: string;
+  Semestre: string;
+  id: number;
+}
 
 export function SchoolRecords() {
+  const [records, setRecords] = useState<Records[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getRecords = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.get('notas/historico/111.111.111-11/ra');
+      setRecords(response.data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    getRecords();
+  }, [getRecords]);
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-primary font-bold text-2xl py-4">
@@ -15,114 +50,65 @@ export function SchoolRecords() {
       </h1>
 
       <div className="w-2/4 h-2/4 bg-primary text-center mb-12">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[250px] text-primary-foreground font-bold">
-                Disciplina: Artes cênicas
-              </TableHead>
-              <TableHead className="text-right text-primary-foreground font-bold">
-                Semestre: 1
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="w-[100px] font-bold">NP1</TableHead>
-              <TableHead className="font-bold">NP2</TableHead>
-              <TableHead className="font-bold">Trabalho</TableHead>
-              <TableHead className="text-right font-bold">
-                Média Final
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                6.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                5.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                7.4
-              </TableCell>
-              <TableCell className="font-bold text-right text-primary-foreground">
-                6.25
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <Table>
-          <TableHeader>
-            <TableRow className="">
-              <TableHead className="w-[250px] text-primary-foreground font-bold">
-                Disciplina: Teatro
-              </TableHead>
-              <TableHead className="text-right text-primary-foreground font-bold">
-                Semestre: 2
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="w-[100px] font-bold">NP1</TableHead>
-              <TableHead className="font-bold">NP2</TableHead>
-              <TableHead className="font-bold">Trabalho</TableHead>
-              <TableHead className="text-right font-bold">
-                Média Final
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                8
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                7
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                6
-              </TableCell>
-              <TableCell className="font-bold text-right text-primary-foreground">
-                7.6
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <Table>
-          <TableHeader>
-            <TableRow className="">
-              <TableHead className="w-[250px] text-primary-foreground font-bold">
-                Disciplina: História da arte
-              </TableHead>
-              <TableHead className="text-right text-primary-foreground font-bold">
-                Semestre: 3
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="w-[100px] font-bold">NP1</TableHead>
-              <TableHead className="font-bold">NP2</TableHead>
-              <TableHead className="font-bold">Trabalho</TableHead>
-              <TableHead className="text-right font-bold">
-                Média Final
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                6.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                5.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                7.4
-              </TableCell>
-              <TableCell className="font-bold text-right text-primary-foreground">
-                6.25
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+        {!records ? (
+          <p className="text-black font-bold text-xl leading-relaxed">
+            Nenhum histórico disponível
+          </p>
+        ) : isLoading ? (
+          <Skeleton className="flex flex-col items-center justify-center w-[960px] h-[600px] space-y-4 bg-gray-800">
+            <Skeleton className="flex flex-col items-center justify-center w-[960px] h-[150px] space-y-2 bg-gray-700">
+              <Skeleton className="w-[300px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+            </Skeleton>
+            <Skeleton className="flex flex-col items-center justify-center w-[800px] h-[120px] space-y-2 bg-gray-700">
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+            </Skeleton>
+          </Skeleton>
+        ) : (
+          records.map((historico) => {
+            return (
+              <Table key={historico.id}>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[250px] text-primary-foreground font-bold">
+                      Disciplina: {historico.disciplina}
+                    </TableHead>
+                    <TableHead className="text-right text-primary-foreground font-bold">
+                      Semestre: {historico.Semestre}
+                    </TableHead>
+                  </TableRow>
+                  <TableRow>
+                    <TableHead className="w-[100px] font-bold">NP1</TableHead>
+                    <TableHead className="font-bold">NP2</TableHead>
+                    <TableHead className="font-bold">Trabalho</TableHead>
+                    <TableHead className="text-right font-bold">
+                      Média Final
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="font-bold text-primary-foreground text-left">
+                      {historico.nota.np1}
+                    </TableCell>
+                    <TableCell className="font-bold text-primary-foreground text-left">
+                      {historico.nota.np2}
+                    </TableCell>
+                    <TableCell className="font-bold text-primary-foreground text-left">
+                      {historico.nota.pim}
+                    </TableCell>
+                    <TableCell className="font-bold text-right text-primary-foreground">
+                      {historico.nota.mf}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            );
+          })
+        )}
       </div>
     </div>
   );
--- web/src/components/school-report/schoolReport.tsx
+++ web/src/components/school-report/schoolReport.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from 'react';
+import { get } from 'lodash';
 import {
   Table,
   TableBody,
@@ -6,115 +8,109 @@ import {
   TableHeader,
   TableRow,
 } from '../ui/table';
+import { api } from '@/lib/api';
+import { Skeleton } from '../ui/skeleton';
+
+interface Report {
+  nota: {
+    np1: number;
+    np2: number;
+    pim: number;
+    mf: number;
+  };
+  disciplina: string;
+  Semestre: string;
+  id: number;
+}
 
 export function SchoolReport() {
+  const [reports, setReports] = useState<Report[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [semestre, setSemestre] = useState(0);
+
+  const getRecords = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.get('notas/boletim/111.111.111-11/ra');
+      const semestre = get(response, 'data[0].Semestre', 0);
+      setSemestre(semestre);
+      setReports(response.data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    getRecords();
+  }, [getRecords]);
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-primary font-bold text-2xl py-4">Boletim Escolar</h1>
 
       <div className="w-2/4 h-2/4 bg-primary text-center mb-12">
         <p className="text-primary-foreground font-bold text-xl">
-          Semestre atual: 1
+          Semestre atual: {semestre}
         </p>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[250px] text-primary-foreground font-bold">
-                Disciplina: Artes cênicas
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="w-[100px] font-bold">NP1</TableHead>
-              <TableHead className="font-bold">NP2</TableHead>
-              <TableHead className="font-bold">Trabalho</TableHead>
-              <TableHead className="text-right font-bold">
-                Média Final
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                6.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                5.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                7.4
-              </TableCell>
-              <TableCell className="font-bold text-right text-primary-foreground">
-                6.25
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[250px] text-primary-foreground font-bold">
-                Disciplina: Artes cênicas
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="w-[100px] font-bold">NP1</TableHead>
-              <TableHead className="font-bold">NP2</TableHead>
-              <TableHead className="font-bold">Trabalho</TableHead>
-              <TableHead className="text-right font-bold">
-                Média Final
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                6.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                5.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                7.4
-              </TableCell>
-              <TableCell className="font-bold text-right text-primary-foreground">
-                6.25
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[250px] text-primary-foreground font-bold">
-                Disciplina: Artes cênicas
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="w-[100px] font-bold">NP1</TableHead>
-              <TableHead className="font-bold">NP2</TableHead>
-              <TableHead className="font-bold">Trabalho</TableHead>
-              <TableHead className="text-right font-bold">
-                Média Final
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                6.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                5.5
-              </TableCell>
-              <TableCell className="font-bold text-primary-foreground text-left">
-                7.4
-              </TableCell>
-              <TableCell className="font-bold text-right text-primary-foreground">
-                6.25
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+        {!reports.length && !isLoading ? (
+          <p className=" text-black font-bold text-xl leading-relaxed">
+            Nenhum Boletim disponível
+          </p>
+        ) : isLoading ? (
+          <Skeleton className="flex flex-col items-center justify-center w-[960px] h-[600px] space-y-4 bg-gray-800">
+            <Skeleton className="flex flex-col items-center justify-center w-[960px] h-[150px] space-y-2 bg-gray-700">
+              <Skeleton className="w-[300px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+            </Skeleton>
+            <Skeleton className="flex flex-col items-center justify-center w-[800px] h-[120px] space-y-2 bg-gray-700">
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+              <Skeleton className="w-[150px] h-[30px] bg-gray-600" />
+            </Skeleton>
+          </Skeleton>
+        ) : (
+          reports.map((report) => {
+            return (
+              <Table key={report.id}>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[250px] text-primary-foreground font-bold">
+                      Disciplina: {report.disciplina}
+                    </TableHead>
+                  </TableRow>
+                  <TableRow>
+                    <TableHead className="w-[100px] font-bold">NP1</TableHead>
+                    <TableHead className="font-bold">NP2</TableHead>
+                    <TableHead className="font-bold">Trabalho</TableHead>
+                    <TableHead className="text-right font-bold">
+                      Média Final
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="font-bold text-primary-foreground text-left">
+                      {report.nota.np1}
+                    </TableCell>
+                    <TableCell className="font-bold text-primary-foreground text-left">
+                      {report.nota.np2}
+                    </TableCell>
+                    <TableCell className="font-bold text-primary-foreground text-left">
+                      {report.nota.pim}
+                    </TableCell>
+                    <TableCell className="font-bold text-right text-primary-foreground">
+                      {report.nota.mf}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            );
+          })
+        )}
       </div>
     </div>
   );
